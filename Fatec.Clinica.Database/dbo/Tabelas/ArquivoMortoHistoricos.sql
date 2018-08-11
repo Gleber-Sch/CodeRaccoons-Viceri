52c345c4f199f207d4312a99e4006b2e83115236
GuilhermeMassao <42052815+GuilhermeMassao@users.noreply.github.com>
2018-08-11 01:38:02 -0300

    V1.5
    Criadas as tabelas do arquivo morto do programa

---
+++ Fatec.Clinica.Database/dbo/Tabelas/ArquivoMortoHistoricos.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE ArquivoMortoHistoricos(
+[Id] INTEGER IDENTITY NOT NULL,
+[Historioco] VARCHAR(300) NOT NULL,
+[IdPaciente] INTEGER NOT NULL,
+CONSTRAINT [FK_ArquivoMortoHistorico] FOREIGN KEY (IdPaciente) REFERENCES [ArquivoMorto] ([Id])
+)
--- Fatec.Clinica.Database/dbo/Tabelas/Horarios.sql
+++ Fatec.Clinica.Database/dbo/Tabelas/Horarios.sql
@@ -4,7 +4,7 @@
 CREATE TABLE Horarios (
 [Id] INTEGER PRIMARY KEY IDENTITY NOT NULL,
 [IdClinica] INTEGER NOT NULL,
-[DiaHora] DATETIME NOT NULL,
+[DiaHora] DATE NOT NULL,
 [Tipoatt] CHAR(1) CHECK ( [Tipoatt] IN ('c' , 'e')) NOT NULL,
 CONSTRAINT [FK_Horarios_Agenda] FOREIGN KEY (IdClinica) REFERENCES [Clinica] ([Id])
 )
