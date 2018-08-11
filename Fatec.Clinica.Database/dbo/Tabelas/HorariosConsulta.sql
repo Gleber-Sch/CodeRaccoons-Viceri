c64d1a729eaf8416c9594f8c147054b21d52bef7
GuilhermeMassao <42052815+GuilhermeMassao@users.noreply.github.com>
2018-08-11 19:39:43 -0300

    V1.5
    Criadas as tabelas de horario exames e horario consulta

---
+++ Fatec.Clinica.Database/dbo/Tabelas/HorariosConsulta.sql
@@ -0,0 +1,11 @@
+﻿/*Cria tabela horarios .*/
+
+
+CREATE TABLE Horarios (
+[Id] INTEGER PRIMARY KEY IDENTITY NOT NULL,
+[Dia] DATE NOT NULL,
+[Hora] TIME NOT NULL,
+[Valor] MONEY NOT NULL,
+[IdAtendimento] INTEGER NOT NULL,
+CONSTRAINT [FK_HorariosConsulta_Atendimento] FOREIGN KEY (IdAtendimento) REFERENCES [Atendimento] ([Id])
+)
