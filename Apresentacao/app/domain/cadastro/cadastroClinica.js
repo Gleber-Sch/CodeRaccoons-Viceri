b5138672ad3235dd7adb7dd32a50e6dfc6ecf6da
Matheus Bio Salles <matheus.biosalles@gmail.com>
2018-08-13 16:34:45 -0300

    Front-End v2.4
    atualização da cadastroClinica.js

--- Apresentacao/app/domain/cadastro/cadastroClinica.js
+++ Apresentacao/app/domain/cadastro/cadastroClinica.js
@@ -1,67 +1,18 @@
 var urlApi = 'http://localhost:53731/api/Clinica/';
-var urlEndereco = 'http://localhost:53731/api/Endereco'
 
-
-var endereco =
-{
-    Estado: document.querySelector('#validationCustom04'),
-    Cidade: document.querySelector('#validationCustom03'),
-    Bairro: document.querySelector('#validationCustom01'),
-    Logradouro: document.querySelector('#validationCustom00'),
-    Numero: document.querySelector('#validationCustom02'),
-    Complemento: document.querySelector('#validationCustom05')
-}
-
-document.querySelector('.form-signin').addEventListener('submit',function(event)
-{
-    event.preventDefault();
-    var objendereco = 
-    {
-        Estado: endereco.Estado.value,
-        Cidade: endereco.Cidade.value,
-        Bairro: endereco.Bairro.value,
-        Logradouro: endereco.Cidade.value,
-        Numero: endereco.Numero.value,
-        Complemento: endereco.Complemento.value
-    }
-
-    inserirEndereco(objendereco);
-});
-
-function inserirEndereco(objendereco)
-{
-    var request = new Request(urlEndereco, 
-        {
-            method: 'POST',
-            headers: new Headers(
-                {
-                    'Content-Type': 'application/json'
-                }),
-                body: JSON.stringify(objendereco)
-        });
-
-        fetch(request)
-        .then(function(response)
-        {
-            alert('Adicionado o Endereço');
-            return response.json(); 
-        })
-        .catch(function(response)
-        {
-            console.log(response);
-        });
-}
-
-
-
-/*
 var clinica = 
 {
     Email: document.querySelector('#login-cli'),
     Senha: document.querySelector('#senha-cli'),
     Nome: document.querySelector('#nome-cli'),
     Cnpj: document.querySelector('#cnpj-cli'),
-    TelefoneCom: document.querySelector('#telefone-cli')
+    TelefoneCom: document.querySelector('#telefone-cli'),
+	Estado: document.querySelector('#validationCustom04'),
+    Cidade: document.querySelector('#validationCustom03'),
+    Bairro: document.querySelector('#validationCustom01'),
+    Logradouro: document.querySelector('#validationCustom00'),
+    Numero: document.querySelector('#validationCustom02'),
+    Complemento: document.querySelector('#validationCustom05')
 };
 
 document.querySelector('.form-signin').addEventListener('submit', function(event)
@@ -74,7 +25,13 @@ document.querySelector('.form-signin').addEventListener('submit', function(event
         Senha: clinica.Senha.value,
         Nome: clinica.Nome.value,
         Cnpj: clinica.Cnpj.value,
-        TelefoneCom: clinica.TelefoneCom.value
+        TelefoneCom: clinica.TelefoneCom.value,
+		Estado: endereco.Estado.value,
+        Cidade: endereco.Cidade.value,
+        Bairro: endereco.Bairro.value,
+        Logradouro: endereco.Cidade.value,
+        Numero: endereco.Numero.value,
+        Complemento: endereco.Complemento.value
     };
 
     inserir(obj);
@@ -104,4 +61,3 @@ function inserir(obj)
             console.log(response);
         })
 }
-*/
