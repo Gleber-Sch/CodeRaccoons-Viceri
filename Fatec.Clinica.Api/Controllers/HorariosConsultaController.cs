374510f394c1c7ac8233db4eb38e63f62a355bdd
GuilhermeMassao <42052815+GuilhermeMassao@users.noreply.github.com>
2018-08-13 04:24:21 -0300

    V1.5
    Horarios Consulta e Exame

---
+++ Fatec.Clinica.Api/Controllers/HorariosConsultaController.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Fatec.Clinica.Negocio;
+using Fatec.Clinica.Dominio;
+using Fatec.Clinica.Dado;
+using System.Net;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Fatec.Clinica.Api.Model;
+
+namespace Fatec.Clinica.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/HorariosConsulta")]
+    public class HorariosConsultaController : Controller
+    {
+        /// <summary>
+        /// Declara as regras de negócio do Horarios de Consulta.
+        /// </summary>
+        private HorariosConsultaNegocio _horariosConsultaNegocio;
+
+        /// <summary>
+        /// Construtor para instanciar as regras de negócio.
+        /// </summary>
+        public HorariosConsultaController()
+        {
+            _horariosConsultaNegocio = new HorariosConsultaNegocio();
+        }
+
+        /// <summary>
+        /// Método que obtêm uma lista com todos os Horarios de Consulta.
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult Get()
+        {
+            return Ok(_horariosConsultaNegocio.Selecionar());
+        }
+
+        /// <summary>
+        /// Método que seleciona um Horario de Consulta.
+        /// </summary>
+        /// <param name="id">Usado para selecionar o Horario.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [Route("{id}", Name = "HorariosConsultaGetId")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult GetId(int id)
+        {
+            return Ok(_horariosConsultaNegocio.SelecionarPorId(id));
+        }
+
+        /// <summary>
+        /// Método que seleciona um Horario de Consulta.
+        /// </summary>
+        /// <param name="dia">Usado para selecionar o Horario.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [Route("Dia/{dia}", Name = "HorariosConsultaGetDia")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult GetDia(DateTime dia)
+        {
+            return Ok(_horariosConsultaNegocio.SelecionarPorDia(dia));
+        }
+
+        /// <summary>
+        /// Método que seleciona um Horario de Consulta.
+        /// </summary>
+        /// <param name="hora">Usado para selecionar o Horario.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [Route("Hora/{hora}", Name = "HorariosConsultaGetHora")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult GetHora(DateTime hora)
+        {
+            return Ok(_horariosConsultaNegocio.SelecionarPorHora(hora));
+        }
+
+        /// <summary>
+        /// Método que seleciona um Horario de Consulta.
+        /// </summary>
+        /// <param name="valor">Usado para selecionar o Horario.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [Route("Valor/{valor}", Name = "HorariosConsultaGetValor")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult GetValor(Decimal valor)
+        {
+            return Ok(_horariosConsultaNegocio.SelecionarPorValor(valor));
+        }
+
+        /// <summary>
+        /// Método que seleciona um Horario de Consulta.
+        /// </summary>
+        /// <param name="id">Usado para selecionar o Horario.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [Route("Atendimento/{id}", Name = "HorariosConsultaGetIdAtendimento")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult GetIdAtendimento(int id)
+        {
+            return Ok(_horariosConsultaNegocio.SelecionarPorIdAtendimento(id));
+        }
+
+        /// <summary>
+        /// Método que seleciona um Horario de Consulta.
+        /// </summary>
+        /// <param name="nome">Usado para selecionar o Horario.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFoud</response>
+        [HttpGet]
+        [Route("Clinica/{nome}", Name = "HorariosConsultaGetNomeClinica")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(HorariosConsulta), nameof(HttpStatusCode.OK))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult GetNomeClinica(string nome)
+        {
+            return Ok(_horariosConsultaNegocio.SelecionarPorNomeClinica(nome));
+        }
+
+        /// <summary>
+        /// Método que insere um novo Horario de Consulta.
+        /// </summary>
+        /// <param name="input">Objeto com os dados do Horario de Consulta.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="400">BadRequest</response>
+        /// <response code="500">InternalServerError</response>
+        [HttpPost]
+        [SwaggerResponse((int)HttpStatusCode.Created, typeof(HorariosConsulta), nameof(HttpStatusCode.Created))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError)]
+        public IActionResult Post([FromBody] HorariosConsultaInput input)
+        {
+            var obj = new HorariosConsulta()
+            {
+                DiaHora = input.DiaHora,
+                Valor = input.Valor,
+                IdAtendimento = input.IdAtendimento
+            };
+
+            var idHorarioConsulta = _horariosConsultaNegocio.Inserir(obj);
+            obj.Id = idHorarioConsulta;
+            return CreatedAtRoute("AtendimentoGetId", new { id = idHorarioConsulta }, obj);
+        }
+
+        /// <summary>
+        /// Método que altera os dados de um Horario de Consulta.
+        /// </summary>
+        /// <param name="id">Usado para buscar o Horario.</param>
+        /// <param name="input">Objeto que contêm os dados a serem alteradas.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="400">BadRequest</response>
+        /// <response code="500">InternalServerError</response>
+        [HttpPut]
+        [Route("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.Accepted, typeof(HorariosConsulta), nameof(HttpStatusCode.Accepted))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError)]
+        public IActionResult Put([FromRoute] int id, [FromBody]HorariosConsultaInput input)
+        {
+            var obj = new HorariosConsulta()
+            {
+                DiaHora = input.DiaHora,
+                Valor = input.Valor,
+                IdAtendimento = input.IdAtendimento
+            };
+            var Obj = _horariosConsultaNegocio.Alterar(id, obj);
+            return Accepted(Obj);
+        }
+
+        /// <summary>
+        /// Método que deleta um Horario de Consulta
+        /// </summary>
+        /// <param name="id">Usado para buscar o Horario de Consulta.</param>
+        /// <returns></returns>
+        /// <response code="200">OK</response>
+        /// <response code="404">NotFound</response>
+        [HttpDelete]
+        [Route("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public IActionResult Delete([FromRoute]int id)
+        {
+            _horariosConsultaNegocio.Deletar(id);
+            return Ok();
+        }
+    }
+}
---
+++ Fatec.Clinica.Negocio/HorariosConsultaNegocio.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using Fatec.Clinica.Dado;
+using Fatec.Clinica.Dominio;
+using Fatec.Clinica.Dominio.Excecoes;
+using Fatec.Clinica.Negocio.Abstracao;
+using Fatec.Clinica.Negocio.Validacoes;
+
+namespace Fatec.Clinica.Negocio
+{
+    /// <summary>
+    /// Regras de Negócio sobre o HorariosConsulta
+    /// </summary>
+    public class HorariosConsultaNegocio : INegocioBase<HorariosConsulta>
+    {
+        /// <summary>
+        /// Declara o repositório do HorariosConsulta.
+        /// </summary>
+        private readonly HorariosConsultaRepositorio _horariosConsultaRepositorio;
+
+        /// <summary>
+        /// Construtor para instanciar o repositório.
+        /// </summary>
+        public HorariosConsultaNegocio()
+        {
+            _horariosConsultaRepositorio = new HorariosConsultaRepositorio();
+        }
+
+        /// <summary>
+        /// Seleciona todas os horarios consulta.
+        /// </summary>
+        /// <returns>Lista de Consultas.</returns>
+        public IEnumerable<HorariosConsulta> Selecionar()
+        {
+            return _horariosConsultaRepositorio.Selecionar();
+        }
+
+
+        /// <summary>
+        /// Verifica se existe algum HorarioConsulta com o ID indicado.
+        /// </summary>
+        /// <param name="id">Usado para buscar um HorarioConsulta no Database.</param>
+        /// <returns>Seleciona um HorarioConsulta ou gera uma exceção.</returns>
+        public HorariosConsulta SelecionarPorId(int id)
+        {
+            var obj = _horariosConsultaRepositorio.SelecionarPorId(id);
+
+            if (obj == null)
+                throw new NaoEncontradoException($"Não foi encontrado um Horario de Consulta com o ID: {id}");
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Verifica se existe algum HorarioConsulta com o Dia indicado.
+        /// </summary>
+        /// <param name="id">Usado para buscar um HorarioConsulta no Database.</param>
+        /// <returns>Seleciona um HorarioConsulta ou gera uma exceção.</returns>
+        public IEnumerable<HorariosConsulta> SelecionarPorDia(DateTime DiaHora)
+        {
+            var lista = _horariosConsultaRepositorio.SelecionarPorDia(DiaHora);
+
+            if (lista == null)
+                throw new NaoEncontradoException($"Não foi encontrado Horario de Consulta com o seguinte dia: {DiaHora.ToShortDateString()}");
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Verifica se existe algum HorarioConsulta com a Hora indicado.
+        /// </summary>
+        /// <param name="id">Usado para buscar um HorarioConsulta no Database.</param>
+        /// <returns>Seleciona um HorarioConsulta ou gera uma exceção.</returns>
+        public IEnumerable<HorariosConsulta> SelecionarPorHora(DateTime DiaHora)
+        {
+            var lista = _horariosConsultaRepositorio.SelecionarPorHora(DiaHora);
+
+            if (lista == null)
+                throw new NaoEncontradoException($"Não foi encontrado Horario de Consulta com o seguinte dia: {DiaHora.ToShortDateString()}");
+
+            return lista;
+        }
+
+
+
+        /// <summary>
+        /// Verifica se existe algum HorarioConsulta com a Hora indicado.
+        /// </summary>
+        /// <param name="id">Usado para buscar um HorarioConsulta no Database.</param>
+        /// <returns>Seleciona um HorarioConsulta ou gera uma exceção.</returns>
+        public IEnumerable<HorariosConsulta> SelecionarPorValor(Decimal Valor)
+        {
+            var lista = _horariosConsultaRepositorio.SelecionarPorValor(Valor);
+
+            if (lista == null)
+                throw new NaoEncontradoException($"Não foi encontrado Horario de Consulta com o seguinte Valor: {Valor}");
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Verifica se existe algum HorarioConsulta com o IDAtendimento indicado.
+        /// </summary>
+        /// <param name="id">Usado para buscar um HorarioConsulta no Database.</param>
+        /// <returns>Seleciona um HorarioConsulta ou gera uma exceção.</returns>
+        public IEnumerable<HorariosConsulta> SelecionarPorIdAtendimento(int IdAtendimento)
+        {
+            var lista = _horariosConsultaRepositorio.SelecionarPorIdAtendimento(IdAtendimento);
+
+
+            if (lista == null)
+                throw new NaoEncontradoException($"Não foi encontrado Horario de Consulta com o seguinte IdAtendimento: {IdAtendimento}");
+
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Verifica se existe algum HorarioConsulta com o NomeClinica indicado.
+        /// </summary>
+        /// <param name="id">Usado para buscar um HorarioConsulta no Database.</param>
+        /// <returns>Seleciona um HorarioConsulta ou gera uma exceção.</returns>
+        public IEnumerable<HorariosConsulta> SelecionarPorNomeClinica(string NomeClinica)
+        {
+            var lista = _horariosConsultaRepositorio.SelecionarPorNomeClinica(NomeClinica);
+
+
+            if (lista == null)
+                throw new NaoEncontradoException($"Não foi encontrado Horario de Consulta com o seguinte NomeClinica: '{NomeClinica}'");
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Verifica o horario ja existe um horario cadastrado no mesmo dia e horario, se o Dia e o horario é válido, se existem campos obrigatórios
+        /// que não estão preenchidos, se os campos respeitam os limites de caracteres especificados no Database.
+        /// </summary>
+        /// <param name="entity">Objeto com os dados do médico.</param>
+        /// <returns>ID do médico inserido no Database ou gera alguma exceção.</returns>
+        public int Inserir(HorariosConsulta entity)
+        {
+            //Verifica se existem campos vazios.
+            if (CamposVazios.Verificar(entity))
+            {
+                throw new DadoInvalidoException("Existem campos obrigatórios que não foram preenchidos!");
+            }
+            List<HorariosConsulta> obj = _horariosConsultaRepositorio.SelecionarPorDiaNegocio(entity.DiaHora);
+            if (obj.Count >0)
+            {
+                var Obj = _horariosConsultaRepositorio.SelecionarPorHoraNegocio(entity.DiaHora);
+                if (Obj.Count>0)
+                    throw new ConflitoException("O horario ja foi marcado como Disponivel!");
+            }
+
+            return _horariosConsultaRepositorio.Inserir(entity);
+        }
+
+
+        /// <summary>
+        /// Verifica o horario ja existe um horario cadastrado no mesmo dia e horario, se o Dia e o horario é válido, se existem campos obrigatórios
+        /// que não estão preenchidos, se os campos respeitam os limites de caracteres especificados no Database.
+        /// </summary>
+        /// <param name="entity">Objeto com os dados do médico.</param>
+        /// <returns>Seleciona um HorarioConsulta do Database ou gera alguma exceção.</returns>
+        public HorariosConsulta Alterar(int id, HorariosConsulta entity)
+        {
+            //Verifica se existem campos vazios.
+            if (CamposVazios.Verificar(entity))
+            {
+                throw new DadoInvalidoException("Existem campos obrigatórios que não foram preenchidos!");
+            }
+
+            List<HorariosConsulta> obj = _horariosConsultaRepositorio.SelecionarPorDiaNegocio(entity.DiaHora);
+            if (obj.Count > 0)
+            {
+                var Obj = _horariosConsultaRepositorio.SelecionarPorHoraNegocio(entity.DiaHora);
+                if (Obj.Count > 0)
+                    throw new ConflitoException("O horario ja foi marcado como Disponivel!");
+            }
+
+            entity.Id = id;
+            _horariosConsultaRepositorio.Alterar(entity);
+            return _horariosConsultaRepositorio.SelecionarPorId(id);
+        }
+
+        /// <summary>
+        /// Verifica se o Horario de Consulta existe no Database antes de deleta-lo.
+        /// </summary>
+        /// <param name="id">Usado para buscar o Horario de Consulta no Database.</param>
+        public void Deletar(int id)
+        {
+            //Verifica se o ID do médico existe.
+            var obj = SelecionarPorId(id);
+            if (obj == null)
+            {
+                throw new NaoEncontradoException($"O ID: \"{id}\" não foi encontrado!");
+            }
+
+            _horariosConsultaRepositorio.Deletar(obj.Id);
+        }
+
+    }
+}
