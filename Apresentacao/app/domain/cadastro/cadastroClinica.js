54101271c090a1bb5c54691fb4e5c2081b58a999
LucasContesini <lucas.contesini@hotmail.com>
2018-08-12 21:27:00 -0300

    v1.30

---
+++ Apresentacao/app/domain/cadastro/cadastroClinica.js
@@ -0,0 +1,11 @@
+var urlApi = 'http://localhost:53731/api/Paciente/';
+
+var clinica = 
+{
+    Email: document.querySelector('#login-cli'),
+    Senha: document.querySelector('#senha-cli'),
+    Nome: document.querySelector('#nome-cli'),
+    Cnpj: document.querySelector('#cnpj-cli'),
+    TelefoneCom: document.querySelector('#telefone-cli'),
+    
+}
