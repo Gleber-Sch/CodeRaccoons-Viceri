6e52d453ed1ca2babd7c3dac19633f7f52c6cf36
LucasContesini <lucas.contesini@hotmail.com>
2018-08-12 22:28:34 -0300

    v1.32

--- Apresentacao/app/domain/cadastro/cadastroClinica.js
+++ Apresentacao/app/domain/cadastro/cadastroClinica.js
@@ -6,6 +6,44 @@ var clinica =
     Senha: document.querySelector('#senha-cli'),
     Nome: document.querySelector('#nome-cli'),
     Cnpj: document.querySelector('#cnpj-cli'),
-    TelefoneCom: document.querySelector('#telefone-cli'),
-    
+    TelefoneCom: document.querySelector('#telefone-cli')
+};
+
+document.querySelector('.form-signin').addEventListener('submit', function(event)
+{
+    event.preventDefault();
+
+    var obj = 
+    {
+        Email: clinica.Email.value,
+        Senha: clinica.Senha.value,
+        Nome: clinica.Nome.value,
+        Cnpj: clinica.Cnpj.value,
+        TelefoneCom: clinica.TelefoneCom.value
+    };
+
+    inserir(obj);
+});
+
+function inserir(obj)
+{
+    var request = new Request(urlApi, 
+        {
+            method: 'POST',
+            headers: new Headers(
+                {
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify(obj)
+        });
+
+        fetch(request)
+        .then(function(response)
+        {
+            return response.json();
+        })
+        .catch(function(response)
+        {
+            console.log(response);
+        })
 }
